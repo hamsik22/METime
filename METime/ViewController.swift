c6e367033d2c10cbdb717a52d4f9dd6d94baef78
HamSik <payback9488@gmail.com>
2023-04-16 21:15:10 +0900

    ➕[Add]: First Commit & Create Project

---
+++ METime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  METime
+//
+//  Created by 황석현 on 2023/04/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
